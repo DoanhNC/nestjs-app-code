1d22188cebd226e147670e8851fcae01796b78a1
doanhnc <doanhelsea@gmail.com>
2026-08-17 14:21:19 +0700

    cập nhật tiếp

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Công Doanh!';
+    return 'Hello World Công Doanh lần tiếp theo!';
   }
 }
